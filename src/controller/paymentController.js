e41ee252ede0c76522556a62bd6ab3661bd6edf1
satriazafarani <satriazafarani57@gmail.com>
2026-08-26 13:50:00 +0700

    create simulate payment controller

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+import Booking from "../models/bookingModels.js";
 import Payment from "../models/paymentModels.js";
 
 export const getPaymentById = async (req, res) => {
@@ -32,5 +34,97 @@ export const getPaymentById = async (req, res) => {
 			.status(500)
 			.json({ message: "Failed get payment ", error: error.message });
 		console.error(error);
+	}
+};
+
+export const simulatePayment = async (req, res) => {
+	const session = await mongoose.startSession();
+	try {
+		const { paymentId } = req.params;
+
+		session.startTransaction();
+
+		const payment = await Payment.findById(paymentId).session(session);
+
+		if (!payment) {
+			return res.status(404).json({ message: "Payment not found" });
+		}
+
+		// Authorization
+		if (payment.user.toString() !== req.user._id.toString()) {
+			await session.abortTransaction();
+			return res.status(403).json({
+				message: "You are not authorized for this payment",
+			});
+		}
+
+		// Payment already paid
+		if (payment.paymentStatus === "Paid") {
+			await session.abortTransaction();
+			return res.status(400).json({
+				message: "Payment has already been paid",
+			});
+		}
+
+		// Payment expired
+		if (payment.paymentStatus === "Expired") {
+			await session.abortTransaction();
+			return res.status(400).json({
+				message: "Payment has been expired",
+			});
+		}
+
+		// Get related booking
+		const booking = await Booking.findById(payment.booking).session(
+			session,
+		);
+
+		if (!booking) {
+			await session.abortTransaction();
+			return res.status(404).json({ message: "Booking not found" });
+		}
+
+		// Booking must be still pending
+		if (booking.bookingStatus !== "Pending") {
+			await session.abortTransaction();
+			return res
+				.status(400)
+				.json({ message: "Booking is not available for payment" });
+		}
+
+		// Check booking expiration
+		if (booking.expiresAt && new Date() > booking.expiresAt) {
+			payment.paymentStatus = "Expired";
+			await payment.save({ session });
+
+			booking.bookingStatus = "Expired";
+			booking.paymentStatus = "Expired";
+			await booking.save({ session });
+
+			await session.commitTransaction();
+
+			return res.status(400).json({ message: "Payment has expired" });
+		}
+
+		// SIMULATE PAYMENT
+		payment.paymentStatus = "Paid";
+		payment.paidAt = new Date();
+
+		booking.paymentStatus = "Paid";
+		booking.bookingStatus = "Confirmed";
+
+		await payment.save({ session });
+		await booking.save({ session });
+
+		await session.commitTransaction();
+
+		res.status(200).json({ message: "Payment succes", payment, booking });
+	} catch (error) {
+		res.statuss(500).json({
+			message: "Payment Failed",
+			erorr: error.message,
+		});
+	} finally {
+		session.endSession();
 	}
 };
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getPaymentById } from "../controller/paymentController.js";
+import {
+	getPaymentById,
+	simulatePayment,
+} from "../controller/paymentController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/:paymentId", protect, getPaymentById);
+router.post("/:paymentId/pay", protect, simulatePayment);
 
 export default router;
